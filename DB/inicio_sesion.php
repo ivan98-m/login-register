dc2d6f7d90274c495b6e833724c63cb931146fb2
Ivan Morales <ivan_98m@outlook.com>
2021-11-05 11:01:45 -0500

    se implemeta el uso de variables de sesion

--- DB/inicio_sesion.php
+++ DB/inicio_sesion.php
@@ -1,15 +1,18 @@
 <?php
+    session_start();
     require 'conexion_db.php';
     if (!empty($_POST['username']) && !empty($_POST['password'])) {
-        $records = $conn->prepare('SELECT * FROM users WHERE user = :username');
+        $records = $conn->prepare('SELECT user, pass, names, lastnames FROM users WHERE user = :username');
         $records->bindParam(':username', $_POST['username']);
         $records->execute();
         $results = $records->fetch(PDO::FETCH_ASSOC);
 
         // if ($results && password_verify($_POST['password'], $results['pass'])) {
         if ($results && md5($_POST['password']) == $results['pass']) {
-            $usuario = $results['names']." ".$results['lastnames'];
-            header("Location: ../prueba.php?respuesta=$usuario");
+            $_SESSION['username']=$results['user'];
+            $_SESSION['infouser'] = $results['names']." ".$results['lastnames'];
+            // $usuario = $results['names']." ".$results['lastnames'];
+            header("Location: ../home.php");
         } else {
             echo'<script type="text/javascript">
             alert("Credenciales incorrectas");
